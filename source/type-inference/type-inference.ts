7f407f29ef0ee81d6053c516f3d0ee638cb4064a
Kamil Wojkowski <wojkowski.kamil@gmail.com>
2023-07-29 13:53:18 +0200

    feat: update inference movie source code

--- source/type-inference/type-inference.ts
+++ source/type-inference/type-inference.ts
@@ -1,7 +1,25 @@
-// export {}
+type ChannelStatus = "UNVERIFIED" | "VERIFIED" | "DISABLED";
+
+type Channel = {
+  name: string;
+  subs: number;
+  alias: string;
+  status: ChannelStatus;
+};
+
+const channel: Channel = {
+  name: "Kod w butach",
+  subs: 30,
+  alias: "@kodwbutach",
+  status: "VERIFIED",
+};
+
+// let key: keyof Channel = ''
 
 function removeProp<T extends object, P extends keyof T>(from: T, prop: P) {
-    const {[prop]: removed, ...rest} = from;
-    
-    return rest;
+  const { [prop]: removed, ...rest } = from;
+
+  return rest;
 }
+
+const noSubs = removeProp(channel, "subs");
